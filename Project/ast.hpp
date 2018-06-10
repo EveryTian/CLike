3ff3ad49fd976d26884b2a386ef5a7c29310a531
zjuzzb <1916317435@qq.com>
2018-06-10 15:39:38 +0800

    update

---
+++ Project/ast.hpp
@@ -0,0 +1,680 @@
+#ifndef AST_H
+#define AST_H
+
+#include <vector>
+#include <string>
+#include <queue>
+
+class Node;
+class Program;
+class Statement;
+class Expression;
+class GlobalStatement;
+class VariableType;
+class GlobalVariableDeclarationStatement;
+class VariableDeclarationStatement;
+class VariableDeclaration;
+class Variable;
+class Identifier;
+// class GlobalFunctionDeclarationStatement;
+class FunctionDeclarationStatement;
+class Parameter;
+class Block;
+class ExpressionStatement;
+class ReturnStatement;
+class WhileStatement;
+class ForStatement;
+class IfStatement;
+class AssignExpression;
+class AndExpression;
+class OrExpression;
+class GtExpression;
+class LtExpression;
+class GeExpression;
+class LeExpression;
+class EqExpression;
+class NeExpression;
+class AddExpression;
+class SubExpression;
+class MulExpression;
+class DivExpression;
+class UnaryExpression;
+class NotExpression;
+class FunctionCallExpression;
+class Argument;
+class Value;
+class IntValue;
+class FloatValue;
+
+// typedef std::vector<GlobalStatement *> GlobalStatementList;
+// typedef std::vector<VariableDeclaration *> VariableDeclarationList;
+typedef std::vector<Parameter *> ParameterList;
+typedef std::vector<VariableDeclarationStatement *> VariableDeclarationStatementList;
+typedef std::vector<Statement *> StatementList;
+typedef std::vector<Argument *> ArgumentList;
+
+class Node {
+public:
+    virtual std::string toString(){
+	}
+	virtual void pushto(std::queue<Node*> *q){
+	}
+};
+
+class Program : public Node {
+public:
+	StatementList *globalStatementList;
+	virtual std::string toString(){
+        return "Program|";
+	}
+    virtual void pushto(std::queue<Node*> *q){
+        if(globalStatementList){
+            for(int i = 0; i < globalStatementList->size(); i++){
+                q->push((Node*)((*globalStatementList)[i]));
+            }
+        }
+	}
+};
+
+class Statement : public Node {
+public:
+    virtual std::string toString(){
+	}
+	virtual void pushto(std::queue<Node*> *q){
+	}
+};
+
+class Expression : public Node {
+public:
+    virtual std::string toString(){
+	}
+	virtual void pushto(std::queue<Node*> *q){
+	}
+};
+
+// class GlobalStatement : Statement {
+// public:
+// };
+
+class VariableType : public Node {
+public:
+	enum VariableTypeEnum {
+		INT = 1, FLOAT = 2, CHAR = 3
+	};
+	VariableTypeEnum variableType;
+	VariableType(int type) {
+        switch (type) {
+        case 1:
+            variableType = INT;
+            break;
+        case 2:
+            variableType = FLOAT;
+            break;
+        case 3:
+            variableType = CHAR;
+            break;
+        }
+	}
+    virtual std::string toString(){
+        switch (variableType) {
+            case INT:
+                return "int|";
+            case FLOAT:
+                return "float|";
+            case CHAR:
+                return "char|";
+        }
+	}
+	virtual void pushto(std::queue<Node*> *q){
+
+	}
+};
+
+// class GlobalVariableDeclarationStatement : GlobalStatement {
+// public:
+// 	VariableType *variableType;
+//     Variable *variable;
+//     Expression *expression;
+//     GlobalVariableDeclarationStatement(VariableType *varType, Variable *var, Expression *expr) {
+//         variableType = varType;
+//         variable = var;
+//         expression = expr;
+//     }
+// };
+
+class VariableDeclarationStatement : public Statement {
+public:
+	VariableType *variableType;
+    Variable *variable;
+    Expression *expression;
+    VariableDeclarationStatement(VariableType *varType, Variable *var, Expression *expr) {
+        variableType = varType;
+        variable = var;
+        expression = expr;
+    }
+    virtual std::string toString(){
+        return "VarDec vType var expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(variableType);
+        q->push((Node*)variable);
+        q->push(expression);
+	}
+};
+
+// class VariableDeclaration : Node {
+// public:
+// 	Variable * variable;
+// 	Expression *expression;
+// };
+
+class Variable : public Expression {
+public:
+	Identifier *identifier;
+	Expression *indexExpression;
+    Variable(Identifier *ident, Expression *indexExpr) {
+        identifier = ident;
+        indexExpression = indexExpr;
+    }
+    virtual std::string toString(){
+        return "id expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push((Node*)identifier);
+        if(indexExpression){
+            q->push(indexExpression);
+        }
+	}
+};
+
+class Identifier : public Expression {
+public:
+	std::string *identifier;
+    Identifier(std::string *ident) {
+        identifier = ident;
+    }
+    virtual std::string toString(){
+        return *identifier + "|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+
+	}
+};
+
+// class GlobalFunctionDeclarationStatement : GlobalStatement {
+class FunctionDeclarationStatement : public Statement {
+public:
+	VariableType *variableType;
+	Identifier *identifier;
+	ParameterList *parameterList;
+	Block *block;
+    FunctionDeclarationStatement(VariableType *varType, Identifier *ident, ParameterList *paraList, Block *blk) {
+        variableType = varType;
+        identifier = ident;
+        parameterList = paraList;
+        block = blk;
+    }
+    virtual std::string toString(){
+        return "FuncDec vType id paraList block|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        if(variableType){
+            q->push(variableType);
+        }
+        q->push(identifier);
+        if(parameterList){
+            for(int i = 0; i < parameterList->size(); i++){
+                q->push((Node*)((*parameterList)[i]));
+            }
+        }
+        if(block){
+            q->push((Node*)block);
+        }
+	}
+};
+
+class Parameter : public Node {
+public:
+	VariableType *variableType;
+	Identifier *identifier;
+    Parameter(VariableType *varType, Identifier *ident) {
+        variableType = varType;
+        identifier = ident;
+    }
+    virtual std::string toString(){
+        return "vType id|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(variableType);
+        q->push((Node*)identifier);
+	}
+};
+
+class Block : public Statement {
+public:
+	VariableDeclarationStatementList *variableDeclarationstatementList;
+	StatementList *statementList;
+    Block(VariableDeclarationStatementList *vdsl, StatementList *sl) {
+        variableDeclarationstatementList = vdsl;
+        statementList = sl;
+    }
+    virtual std::string toString(){
+        return "varDecStmtList stmtList|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        if(variableDeclarationstatementList){
+            for(int i = 0; i < variableDeclarationstatementList->size(); i++){
+                q->push((Node*)((*variableDeclarationstatementList)[i]));
+            }
+        }
+        if(statementList){
+            for(int i = 0; i < statementList->size(); i++){
+                q->push((Node*)((*statementList)[i]));
+            }
+        }
+	}
+};
+
+class ExpressionStatement : public Statement {
+public:
+	Expression *expression;
+    ExpressionStatement(Expression *e) {
+        expression = e;
+    }
+    virtual std::string toString(){
+        return "expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+	}
+};
+
+class ReturnStatement : public Statement {
+public:
+	Expression *expression;
+    ReturnStatement(Expression *e) {
+        expression = e;
+    }
+    virtual std::string toString(){
+        return "return expr|";
+	}
+    virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+	}
+};
+
+class WhileStatement : public Statement {
+public:
+	Expression *expression;
+	Statement *statement;
+    WhileStatement(Expression *expr, Statement* stmt) {
+        expression = expr;
+        statement = stmt;
+    }
+    virtual std::string toString(){
+        return "while expr stmt|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+        q->push(statement);
+	}
+};
+
+class ForStatement : public Statement {
+public:
+	Expression *expression1;
+	Expression *expression2;
+	Expression *expression3;
+	Statement *statement;
+    ForStatement(Expression *expr1, Expression *expr2, Expression *expr3, Statement *stmt) {
+        expression1 = expr1;
+        expression2 = expr2;
+        expression3 = expr3;
+        statement = stmt;
+    }
+    virtual std::string toString(){
+        return "for expr expr expr stmt|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+        q->push(expression3);
+        q->push(statement);
+	}
+};
+
+class IfStatement : public Statement {
+public:
+	Expression *expression;
+	Statement *ifStatement;
+	Statement *elseStatement;
+    IfStatement(Expression *e, Statement *ts, Statement *es) {
+        expression = e;
+        ifStatement = ts;
+        elseStatement = es;
+    }
+    virtual std::string toString(){
+        return "if expr stmt else stmt|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+        q->push(ifStatement);
+        if(elseStatement){
+            q->push(elseStatement);
+        }
+	}
+};
+
+class AssignExpression : public Expression {
+public:
+	Variable *variable;
+	Expression *expression;
+    AssignExpression(Variable *v, Expression *e) {
+        variable = v;
+        expression = e;
+    }
+    virtual std::string toString(){
+        return "assign var expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(variable);
+        q->push(expression);
+	}
+};
+
+class AndExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    AndExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr and expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class OrExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    OrExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr or expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class GtExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    GtExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr gt expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class LtExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    LtExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr lt expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class GeExpression : public Expression {
+public:
+	Expression * expression1;
+	Expression *expression2;
+    GeExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr ge expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class LeExpression : public Expression {
+public:
+	Expression * expression1;
+	Expression *expression2;
+    LeExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr le expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class EqExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    EqExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr eq expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class NeExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    NeExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr ne expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class AddExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    AddExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr + expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class SubExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    SubExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr - expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class MulExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    MulExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr * expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class DivExpression : public Expression {
+public:
+	Expression *expression1;
+	Expression *expression2;
+    DivExpression(Expression *e1, Expression *e2) {
+        expression1 = e1;
+        expression2 = e2;
+    }
+    virtual std::string toString(){
+        return "expr / expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression1);
+        q->push(expression2);
+	}
+};
+
+class UnaryExpression : public Expression {
+public:
+	Expression * expression;
+    UnaryExpression(Expression* e) {
+        expression = e;
+    }
+    virtual std::string toString(){
+        return "-expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+	}
+};
+
+class NotExpression : public Expression {
+public:
+	Expression * expression;
+    NotExpression(Expression* e) {
+        expression = e;
+    }
+    virtual std::string toString(){
+        return "!expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+	}
+};
+
+class FunctionCallExpression : public Expression {
+public:
+	Identifier *identifier;
+	ArgumentList *argumentList;
+    FunctionCallExpression(Identifier *ident, ArgumentList *argList) {
+        identifier = ident;
+        argumentList = argList;
+    }
+    virtual std::string toString(){
+        return "call id argList|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(identifier);
+        if(argumentList){
+            for(int i = 0; i < argumentList->size(); i++){
+                q->push((Node*)((*argumentList)[i]));
+            }
+        }
+	}
+};
+
+class Argument : public Expression {
+public:
+	Expression *expression;
+    Argument(Expression *expr) {
+        expression = expr;
+    }
+    virtual std::string toString(){
+        return "expr|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+        q->push(expression);
+	}
+};
+
+class Value : public Expression {
+public:
+};
+
+class IntValue : public Value {
+public:
+	int value;
+	IntValue(int v) { value = v; }
+	virtual std::string toString(){
+        return std::to_string(value)+"|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+
+	}
+};
+
+class FloatValue : public Value {
+public:
+	float value;
+	FloatValue(float v) { value = v; }
+	virtual std::string toString(){
+        return std::to_string(value)+"|";
+	}
+	virtual void pushto(std::queue<Node*> *q){
+
+	}
+};
+
+#endif
---
+++ Project/clike.yy
@@ -0,0 +1,344 @@
+%{
+
+#include "ast.hpp"
+#include <string>
+#include <cstdlib>
+#include <stdio.h>
+
+Program *program;
+extern int yylex();
+void yyerror(const char *s) { 
+    printf("Error: %s\n", s);
+    exit(1); 
+}
+
+%}
+
+%token <string> INT_VALUE FLOAT_VALUE IDENT_VALUE
+%token LCB RCB LP RP LB RB SQ DQ
+%token ADD SUB MUL DIV ASSIGN EQ NE GT GE LT LE AND OR NOT
+%token IF ELSE DO WHILE FOR BREAK CONTINUE RETURN
+%token VOID INT FLOAT CHAR
+%token SEMI COMMA
+
+%union {
+    Node                            *node;
+    Program                         *prog;
+    Statement                       *stmt;
+    Expression                      *expr;
+
+    // GlobalStatement *glbstmt;
+    // GlobalVariableDeclarationStatement *glbvardecstmt;
+    // GlobalFunctionDeclarationStatement *glbfundecstmt;
+    FunctionDeclarationStatement    *glbfundecstmt;
+
+    VariableDeclarationStatement    *vardecstmt;
+    VariableType                    *vartype;
+    VariableDeclaration             *vardec;
+    Variable                        *var;
+    Identifier                      *idt;
+    Parameter                       *para;
+    Block                           *block;
+    ExpressionStatement             *exprstmt;
+    Argument                        *arg;
+
+    // GlobalStatementList *glbstmtls;
+    
+    // VariableDeclarationList *vardecls;
+    ParameterList *parals;
+    VariableDeclarationStatementList *vardecstmtls;
+    StatementList *stmtls;
+    ArgumentList *argls;
+    Value *value;
+    std::string *string;
+}
+
+%type <node>            program
+// %type <glbstmtls> global_statement_list
+// %type <glbstmt> global_statement function_declaration_staglobal_variable_declaration_statement
+%type <stmt>            statement global_statement function_declaration_statement
+%type <block>           block
+%type <vartype>         variable_type
+// %type <vardecls> variable_declaration_list
+%type <idt>             identifier
+%type <parals>          parameter_list
+%type <vardecstmt>      variable_declaration_statement
+%type <vardecstmtls>    variable_declaration_statement_list
+%type <stmtls>          statement_list global_statement_list
+%type <para>            parameter
+// %type <vardec> variable_declaration
+%type <var>             variable
+%type <expr>            expression
+%type <value>           number
+%type <argls>           argument_list
+%type <arg>             argument
+
+%right '='
+%left OR
+%left AND
+%left ADD SUB
+%left MUL DIV
+%right UPLUS UMINUS
+%right NOT
+%left RP RB LP LB
+%nonassoc NOELSE
+%nonassoc ELSE
+
+%start program
+
+%%
+
+program: global_statement_list {
+        program = new Program();
+        program->globalStatementList = $1;
+    }
+    ;
+
+global_statement_list: global_statement {
+        // $$ = new GlobalStatementList();
+        $$ = new StatementList();
+        $$->push_back($1);
+    }
+    | global_statement_list global_statement {
+        $1->push_back($2);
+    }
+    ;
+
+global_statement:
+    variable_declaration_statement {
+        $$ = $1; 
+    }
+    | function_declaration_statement {
+        $$ = $1;
+    }
+    ;
+
+variable_declaration_statement:
+    variable_type variable ASSIGN expression SEMI {
+        $$ = new VariableDeclarationStatement($1, $2, $4);
+    }
+    ;
+
+function_declaration_statement:
+    variable_type identifier LP parameter_list RP block {
+        // $$ = new GlobalFunctionDeclarationStatement($1, $2, $4, $6);
+        $$ = new FunctionDeclarationStatement($1, $2, $4, $6);
+    }
+    // | variable_type LB RB identifier
+    | VOID identifier LP parameter_list RP block {
+        // $$ = new GlobalFunctionDeclarationStatement(NULL, $2, $4, $6);
+        $$ = new FunctionDeclarationStatement(NULL, $2, $4, $6);
+    }
+    ;
+
+variable_type:
+    INT {
+        $$ = new VariableType(1);
+    }
+    | FLOAT {
+        $$ = new VariableType(2);
+    }
+    | CHAR {
+        $$ = new VariableType(3);
+    }
+    ;
+
+// variable_declaration_list: variable_declaration {}
+//     | variable_declaration COMMA variable_declaration_list {}
+//     ;
+
+identifier:
+    IDENT_VALUE {
+        $$ = new Identifier($1);
+    }
+    ;
+
+parameter_list:
+    parameter {
+        $$ = new ParameterList();
+        $$->push_back($1);
+    }
+    | parameter_list COMMA parameter {
+        $1->push_back($3);
+    }
+    | VOID {
+        $$ = NULL;
+    }
+    | {
+        $$ = NULL;
+    } // The same to VOID.
+    ;
+
+block:
+    LCB RCB {
+        $$ = NULL;
+    }
+    | LCB statement_list RCB {
+        $$ = new Block(NULL, $2);
+    }
+    | LCB variable_declaration_statement_list statement_list RCB {
+        $$ = new Block($2, $3);
+    }
+    ;
+
+variable_declaration_statement_list:
+    variable_declaration_statement {
+        $$ = new VariableDeclarationStatementList();
+        $$->push_back($1);
+    }
+    | variable_declaration_statement_list variable_declaration_statement {
+        $1->push_back($2);
+    }
+    ;
+
+statement_list:
+    statement {
+        $$ = new StatementList();
+        $$->push_back($1);
+    }
+    | statement_list statement {
+        $1->push_back($2);
+    }
+    ;
+
+parameter:
+    variable_type identifier {
+        $$ = new Parameter($1, $2);
+    }
+    ;
+
+// variable_declaration: variable ASSIGN expression {}
+//     | variable {}
+//     ;
+
+variable:
+    identifier {
+        $$ = new Variable($1, NULL);
+    }
+    | identifier LB expression RB {
+        $$ = new Variable($1, $3);
+    }
+    ;
+
+statement:
+    expression SEMI {
+        $$ = new ExpressionStatement($1);
+    }
+    | block {
+        $$ = $1;
+    }
+    | RETURN expression SEMI {
+        $$ = new ReturnStatement($2);
+    }
+    | WHILE LP expression RP statement {
+        $$ = new WhileStatement($3, $5);
+    }
+    | FOR LP expression SEMI expression SEMI expression RP statement {
+        $$ = new ForStatement($3, $5, $7, $9);
+    }
+    | IF LP expression RP statement %prec NOELSE {
+        $$ = new IfStatement($3, $5, NULL);
+    }
+    | IF LP expression RP statement ELSE statement {
+        $$ = new IfStatement($3, $5, $7);
+    }
+    ;
+
+expression:
+    variable ASSIGN expression {
+        $$ = new AssignExpression($1, $3);
+    }
+    | expression AND expression {
+        $$ = new AndExpression($1, $3);
+    }
+    | expression OR expression {
+        $$ = new OrExpression($1, $3);
+    }
+    | expression GT expression {
+        $$ = new GtExpression($1, $3);
+    }
+    | expression LT expression {
+        $$ = new LtExpression($1, $3);
+    }
+    | expression GE expression {
+        $$ = new GeExpression($1, $3);
+    }
+    | expression LE expression {
+        $$ = new LeExpression($1, $3);
+    }
+    | expression EQ expression {
+        $$ = new EqExpression($1, $3);
+    }
+    | expression NE expression {
+        $$ = new NeExpression($1, $3);
+    }
+    | expression ADD expression {
+        $$ = new AddExpression($1, $3);
+    }
+    | expression SUB expression {
+        $$ = new SubExpression($1, $3);
+    }
+    | expression MUL expression {
+        $$ = new MulExpression($1, $3);
+    }
+    | expression DIV expression {
+        $$ = new DivExpression($1, $3);
+    }
+    | LP expression RP {
+        $$ = $2;
+    }
+    | SUB expression %prec UMINUS {
+        $$ = new UnaryExpression($2);
+    }
+    | ADD expression %prec UPLUS {
+        $$ = $2;
+    }
+    | NOT expression {
+        $$ = new NotExpression($2);
+    }
+    // TODO: Use identifier or variable?
+    // If use variable, ast.hpp should be changed.
+    | identifier LP argument_list RP {
+        $$ = new FunctionCallExpression($1, $3);
+    }
+    | number {
+        $$ = $1;
+    }
+    | variable {
+        $$ = $1;
+    }
+    ;
+
+number: 
+    INT_VALUE { 
+        $$ = new IntValue(atoi($1->c_str()));
+        delete $1;
+    }
+    | FLOAT_VALUE {
+        $$ = new FloatValue(atof($1->c_str()));
+        delete $1;
+    }
+    ;
+
+argument_list: 
+    argument {
+        $$ = new ArgumentList();
+        $$->push_back($1);
+    }
+    | argument_list COMMA argument {
+        $1->push_back($3);
+    }
+    | {
+        $$ = NULL;
+    }
+    ;
+
+argument:
+    expression {
+        $$ = new Argument($1);
+    }
+    ;
+    
+%%
+
+
